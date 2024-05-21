e3b0fc88ee70c891a752c91d46aa00794933fc3c
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-05-21 23:36:41 +0400

    New DTOs Added

---
+++ EventTicketAPI/Dtos/ResetPasswordDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventTicketAPI.Dtos
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        public string Token {  get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+}
---
+++ EventTicketAPI/Dtos/UserReturnDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EventTicketAPI.Dtos
+{
+    public class UserReturnDto
+    {
+        public  string? Name { get; set; } 
+        public  string? LastName { get; set; }
+    }
+}
