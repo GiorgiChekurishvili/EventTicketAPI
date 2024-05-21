590718fc4d3deea0b21af91e54e304b8abcb2f62
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-05-21 23:36:52 +0400

    UserRegisterDto Modified

--- EventTicketAPI/Dtos/UserRegisterDto.cs
+++ EventTicketAPI/Dtos/UserRegisterDto.cs
@@ -9,6 +9,7 @@ namespace EventTicketAPI.Dtos
         public required string LastName { get; set; }
         [EmailAddress]
         public required string Email { get; set; }
+
         public required string Password { get; set; }
         [Compare("Password")]
         public required string ConfirmPassword { get; set; }
