e6fa8e707b7b9d699b77df2608a3e4c345cf9117
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-06-13 01:20:47 +0400

    added new service for transactions

---
+++ EventTicketAPI/Services/ITransactionService.cs
@@ -0,0 +1,12 @@
+﻿using EventTicketAPI.Dtos.Transactions;
+using EventTicketAPI.Entities;
+
+namespace EventTicketAPI.Services
+{
+    public interface ITransactionService
+    {
+        FillTransactionsDto MakeTransaction(FillTransactionsDto transaction);
+        decimal CheckBalance(int userId);
+        IEnumerable<TransactionsReturnDto> ShowMyTransactions(int userid);
+    }
+}
---
+++ EventTicketAPI/Services/TransactionService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using EventTicketAPI.Dtos.Transactions;
+using EventTicketAPI.Entities;
+using EventTicketAPI.Repositories;
+
+namespace EventTicketAPI.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly IMapper _mapper;
+        public TransactionService(ITransactionRepository transactionRepository, IMapper mapper)
+        {
+            _transactionRepository = transactionRepository;
+            _mapper = mapper;
+        }
+        public decimal CheckBalance(int userId)
+        {
+            decimal balance = _transactionRepository.CheckMyBalance(userId);
+            return balance;
+        }
+
+        public FillTransactionsDto MakeTransaction(FillTransactionsDto transaction)
+        {
+            var map = _mapper.Map<Transactions>(transaction);
+            var result = _transactionRepository.MakeTransaction(map);
+            
+            return _mapper.Map<FillTransactionsDto>(result);
+        }
+
+        public IEnumerable<TransactionsReturnDto> ShowMyTransactions(int userid)
+        {
+            var data = _transactionRepository.ViewMyTransactions(userid);
+            var map = _mapper.Map<IEnumerable<TransactionsReturnDto>>(data);
+            return map;
+        }
+    }
+}
