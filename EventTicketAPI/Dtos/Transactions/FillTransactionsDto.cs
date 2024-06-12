f59ef5afc5698e2fcf3efa6b4f38919aeaf8c74a
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-06-13 01:42:12 +0400

    added requiered  annotations

--- EventTicketAPI/Dtos/Transactions/FillTransactionsDto.cs
+++ EventTicketAPI/Dtos/Transactions/FillTransactionsDto.cs
@@ -1,9 +1,14 @@
-﻿namespace EventTicketAPI.Dtos.Transactions
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventTicketAPI.Dtos.Transactions
 {
     public class FillTransactionsDto
     {
+        [Required]
         public int UserId { get; set; }
+        [Required]
         public decimal Amount { get; set; }
+        [Required]
         public string Reason { get; set; }
     }
 }
--- EventTicketAPI/Dtos/Transactions/TransactionsReturnDto.cs
+++ EventTicketAPI/Dtos/Transactions/TransactionsReturnDto.cs
@@ -1,9 +1,17 @@
-﻿namespace EventTicketAPI.Dtos.Transactions
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventTicketAPI.Dtos.Transactions
 {
     public class TransactionsReturnDto
     {
+        [Required]
         public decimal Amount { get; set; }
+        [Required]
         public string Reason { get; set; }
+        [Required]
+        public decimal BalanceChanges { get; set; }
+        [Required]
         public DateTime TransactionMade { get; set; }
+
     }
 }
