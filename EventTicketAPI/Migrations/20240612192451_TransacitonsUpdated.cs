4129c409600e2038b28418a87db192afda17346b
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-06-13 01:19:48 +0400

    added modified entities into database

---
+++ EventTicketAPI/Migrations/20240612192451_TransacitonsUpdated.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventTicketAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransacitonsUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Users",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Reason",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Reason",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Transactions",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
